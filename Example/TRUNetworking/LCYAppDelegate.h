c8c5911121aa1ecc3cb213f291ad320fbf20861e
chuangliu133130 <chuangliu133130@126.com>
2020-08-14 11:24:10 +0800

    Initial commit

---
+++ Example/TRUNetworking/LCYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LCYAppDelegate.h
+//  TRUNetworking
+//
+//  Created by chuangliu133130@126.com on 08/14/2020.
+//  Copyright (c) 2020 chuangliu133130@126.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LCYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
